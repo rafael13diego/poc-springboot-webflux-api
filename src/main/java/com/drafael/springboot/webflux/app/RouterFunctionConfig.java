c919ecb92c4263637ac98a5225f1de0983ccbf9f
DiegoRafael <drafael.flomar@gmail.com>
2021-12-17 23:55:32 -0500

    finish functional endpoints and validations

--- src/main/java/com/drafael/springboot/webflux/app/RouterFunctionConfig.java
+++ src/main/java/com/drafael/springboot/webflux/app/RouterFunctionConfig.java
@@ -20,7 +20,12 @@ public class RouterFunctionConfig {
 
        return route(GET("/api/v2/productos").or(GET("/api/v3/productos")),
                handler::listar)
-               .andRoute(GET("/api/v2/productos/{id}"),handler::ver);
+               .andRoute(GET("/api/v2/productos/{id}"),handler::ver)
+               .andRoute(POST("/api/v2/productos"),handler::crear)
+               .andRoute(PUT("/api/v2/productos/{id}"), handler::editar)
+               .andRoute(DELETE("/api/v2/productos/{id}"), handler::eliminar)
+               .andRoute(POST("/api/v2/productos/upload/{id}"), handler::upload)
+               .andRoute(POST("/api/v2/productos/crear"), handler::crearConFoto);
                //.andRoute(GET("/api/v2/productos/{id}").and(contentType(MediaType.APPLICATION_JSON)),handler::ver);
     }
 
