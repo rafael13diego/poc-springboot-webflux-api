9f00c513d0853719397dd0fa5ee9423ee2af9bdb
DiegoRafael <drafael.flomar@gmail.com>
2022-04-07 23:00:25 -0500

    implement eureka-client

--- src/main/java/com/drafael/springboot/webflux/app/SpringBootWebfluxApirestApplication.java
+++ src/main/java/com/drafael/springboot/webflux/app/SpringBootWebfluxApirestApplication.java
@@ -10,11 +10,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
 import reactor.core.publisher.Flux;
 
 import java.util.Date;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class SpringBootWebfluxApirestApplication implements CommandLineRunner {
 
